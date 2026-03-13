61b37dab5956babc7db64d2118758777af22dbce
ConnorJWheatley <connorjwheatley12@gmail.com>
2026-03-13 16:46:52 +0000

    Making restart button in pause menu functional

--- scenes/menus/pause_menu.gd
+++ scenes/menus/pause_menu.gd
@@ -9,7 +9,8 @@ func _on_quit_to_main_menu_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
 func _on_restart_btn_pressed() -> void:
-	pass # Replace with function body.
+	_on_resume_btn_pressed()
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_btn_pressed() -> void:
 	pass # Replace with function body.
